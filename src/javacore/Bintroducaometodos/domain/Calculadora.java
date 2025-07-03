76205dcd39894b3b85daa18b19ca1170f8f2b512
nicolasfreitas-dev <nicolasfpdev@gmail.com>
2025-07-03 12:25:45 -0300

    inicio da revisao de metodos em java

---
+++ src/javacore/Bintroducaometodos/domain/Calculadora.java
@@ -0,0 +1,7 @@
+package javacore.Bintroducaometodos.domain;
+
+public class Calculadora {
+    public void somaDoisNumeros() {
+        System.out.println(10 + 10);
+    }
+}
---
+++ src/javacore/Bintroducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,11 @@
+package javacore.Bintroducaometodos.test;
+
+import javacore.Bintroducaometodos.domain.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.somaDoisNumeros();
+    }
+}

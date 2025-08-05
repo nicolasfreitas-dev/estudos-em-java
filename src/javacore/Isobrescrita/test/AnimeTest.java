382da4bd0861d1d345dbe1f26824126b92f7f9d3
Nicolas <nicolasfpdev@gmail.com>
2025-08-05 12:32:05 -0300

    revisao sobre sobrescrita como toString

---
+++ src/javacore/Isobrescrita/test/AnimeTest.java
@@ -0,0 +1,11 @@
+package javacore.Isobrescrita.test;
+
+import javacore.Isobrescrita.domain.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Naruto");
+
+        System.out.println(anime);
+    }
+}
